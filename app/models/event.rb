e9852ec517794a77ced981683142a89aa5fe6eb7
Mrudhukar <mrudhu@Mrudhukar.(none)>
2009-11-07 18:35:26 +0530

    Changing the db type

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,12 @@
 class Event < ActiveRecord::Base
   EventTypes = [
     "Prime ministers",
-    "Presidents"
+    "Presidents",
+    "Other"
   ]
   validates_presence_of :name, :from, :to, :event_type
+
+  def self.find_by_type(event_type)
+    self.find(:all, :conditions => {:event_type => event_type}, :order => "from ASC")
+  end
 end
